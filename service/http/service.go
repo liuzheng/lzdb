1bde667e2d882f1722a1912ce24dc1d7fdf682f7
liuzheng712 <liuzheng712@gmail.com>
2017-09-16 14:17:11 +0800

    update

--- service/http/service.go
+++ service/http/service.go
@@ -2,18 +2,26 @@ package http
 
 import (
 	log "github.com/liuzheng712/golog"
+	"github.com/liuzheng712/lzdb/service/conf"
 	"net/http"
 	"fmt"
-	"github.com/liuzheng712/lzdb/service/conf"
 )
 
 func init() {
 }
+
 func router() {
 	log.Debug("service.http.router", "%v", "load the Routers")
+	http.HandleFunc("/test", test)
 	http.HandleFunc("/query", queryHandler)
 }
+
+func httpHandler(w http.ResponseWriter) {
+	w.Header().Set("Server", "LZDB Server")
+}
+
 func queryHandler(w http.ResponseWriter, r *http.Request) {
+	httpHandler(w)
 	log.Info("http", "%v\t%v\t%v", r.RemoteAddr, r.Method, r.RequestURI)
 	log.Debug("http", "Header: %v ", r.Header)
 	log.Debug("http", "Cookies: %v ", r.Cookies())
@@ -21,12 +29,46 @@ func queryHandler(w http.ResponseWriter, r *http.Request) {
 
 	switch r.Method {
 	case "GET":
-		fmt.Fprint(w, "xxx")
+		w.Write([]byte("xxx"))
+	case "HEAD":
 	case "POST":
 		log.Debug("http", "Form: %v ", r.Form)
 		log.Debug("http", "PostForm: %v ", r.PostForm)
+	case "OPTIONS":
+	case "PUT":
+	case "DELETE":
+	case "TRACE":
+	case "CONNECT":
+	default:
+		w.WriteHeader(HTTPCodeForbidden)
+	}
+}
+func test(w http.ResponseWriter, r *http.Request) {
+	httpHandler(w)
+	log.Info("http", "%v\t%v\t%v", r.RemoteAddr, r.Method, r.RequestURI)
 
-
+	switch r.Method {
+	case "GET":
+		w.WriteHeader(HTTPCodeForbidden)
+		w.Write([]byte(r.Method))
+	case "HEAD":
+		w.WriteHeader(HTTPCodeForbidden)
+		w.Write([]byte(r.Method))
+	case "POST":
+		log.Debug("http", "Form: %v ", r.Form)
+		log.Debug("http", "PostForm: %v ", r.PostForm)
+	case "OPTIONS":
+		w.Write([]byte(r.Method))
+	case "PUT":
+		w.Write([]byte(r.Method))
+	case "DELETE":
+		w.Write([]byte(r.Method))
+	case "TRACE":
+		w.Write([]byte(r.Method))
+	case "CONNECT":
+		w.Write([]byte(r.Method))
+	default:
+		w.WriteHeader(HTTPCodeForbidden)
 	}
 }
 
